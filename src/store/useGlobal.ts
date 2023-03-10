a2654648bd06b5c23d3b95e6c08b888009786176
aditya <a.dewaskar@gmail.com>
2023-03-10 10:21:45 +0530

    used types

--- src/store/useGlobal.ts
+++ src/store/useGlobal.ts
@@ -1,21 +1,28 @@
 import { GetOrganisationDetails } from '@User/Api'
 import { INITIAL_ORG_DETAILS } from 'constant.ts'
-import { Organisation } from '@Types/Organisation'
+import { Types } from '@adewaskar/lms-common'
+// import { Organisation } from '@Types/Organisation'
 import create from 'zustand'
 
+// console.log(goodBye, 'goodBye')
 interface GlobalState {
-  organisation: Organisation;
-  setOrganisation: (organisation: Organisation) => void,
-  fetchOrganisation: (id:string)=>Promise<void>
-  }
-
-const useGlobal = create<GlobalState>(set => ({
-  organisation: INITIAL_ORG_DETAILS,
-  setOrganisation: (organisation) => set(state => ({ organisation: organisation })),
-  fetchOrganisation:async (orgId:string) => {
-    await GetOrganisationDetails(orgId)
-        .then(data => set({organisation: data}))
+  organisation: Types.Organisation;
+  setOrganisation: (organisation: Types.Organisation) => void;
+  fetchOrganisation: (id: string) => Promise<void>;
 }
-}))
+
+const useGlobal =
+  create <
+  GlobalState >
+  (set => ({
+    organisation: INITIAL_ORG_DETAILS,
+    setOrganisation: organisation =>
+      set(state => ({ organisation: organisation })),
+    fetchOrganisation: async (orgId: string) => {
+      await GetOrganisationDetails(orgId).then(data =>
+        set({ organisation: data })
+      )
+    }
+  }))
 
 export default useGlobal
