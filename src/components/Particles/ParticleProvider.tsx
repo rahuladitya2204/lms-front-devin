331a09fd2e76fa991f12e8f293f6d766ee76186c
aditya <a.dewaskar@gmail.com>
2023-12-03 19:36:33 +0530

    Adding papers now

--- src/components/Particles/ParticleProvider.tsx
+++ src/components/Particles/ParticleProvider.tsx
@@ -34,9 +34,78 @@ export const ParticlesProvider = ({ children }: ParticlesProviderPropsI) => {
           loaded={particlesLoaded}
           // @ts-ignore
           options={particlesConfig}
+          // options={EXAMPLECONFIG}
         />
       </div>
       {children}
     </ParticlesContext.Provider>
   )
+}
+
+const EXAMPLECONFIG = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: ['#ffd700', '#00fde0', '#ffff00']
+    },
+    shape: {
+      type: ['circle', 'triangle', 'star'],
+      stroke: {
+        width: 0
+      }
+    },
+    opacity: {
+      value: 0.6,
+      random: false
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffeb3b',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false
+    },
+    interactivity: {
+      detect_on: 'canvas',
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'repulse'
+        },
+        onclick: {
+          enable: true,
+          mode: 'push'
+        }
+      },
+      modes: {
+        repulse: {
+          distance: 100,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        }
+      }
+    }
+  },
+  retina_detect: true
 }
