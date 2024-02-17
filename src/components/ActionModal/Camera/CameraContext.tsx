35bf1618bba82db2d3714ce5fbe04d2c75d01c0a
aditya <a.dewaskar@gmail.com>
2024-02-18 01:01:28 +0530

    Added multiple image support

--- src/components/ActionModal/Camera/CameraContext.tsx
+++ src/components/ActionModal/Camera/CameraContext.tsx
@@ -1,121 +1,124 @@
+// @ts-nocheck
 import { Alert, Button, Modal } from 'antd';
 import { Camera, CameraType } from 'react-camera-pro';
 import { CameraOutlined, CheckOutlined, CloseOutlined, WarningOutlined } from '@ant-design/icons';
-import React, { createContext, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { createContext, useCallback, useContext, useRef, useState } from 'react';
 import { blobToFile, compressImage } from '@User/Screens/Courses/CourseEditor/CourseBuilder/utils';
 
 import { highlightQuadrilateral } from './highlight-quadrilateral';
-// @ts-ignore
-import { useOpenCv } from 'opencv-react';
-
-// Define the context and its type
-interface CameraContextType {
-  openCamera: () => Promise<File>;
-}
-
-interface CameraProviderPropsI {
-    children:React.ReactNode
-    enableQuadrilateralHighlighting?: boolean;
-  }
 
 const CameraContext = createContext<CameraContextType | undefined>(undefined);
 
-// Camera Provider component
+interface CameraContextType {
+  openCamera: (multiple?: boolean) => Promise<File[]>;
+}
+
+interface CameraProviderPropsI {
+  children: React.ReactNode;
+  enableQuadrilateralHighlighting?: boolean;
+}
+
 export const CameraProvider = ({ children, enableQuadrilateralHighlighting }: CameraProviderPropsI) => {
   const cameraRef = useRef<CameraType>(null);
-  const [facingMode, setFacingMode] = useState('environment');
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [highlightedImage, setHighlightedImage] = useState<string | null>(null);
-  const [previewImage, setPreviewImage] = useState<string | null>(null); // Store captured image
-  const [resolveCapture, setResolveCapture] = useState<(blob: Blob | null) => void>(() => {});
-  const handleCapture = useCallback(async () => {
-    let imageUrl = await cameraRef.current?.takePhoto();
-    // const testUrl=await imageUrlToDataUrl(`https://upload-junk.s3.us-west-2.amazonaws.com/6368e34a86402abb8d2737a9/noprefix/1705772455227.png`)
-    // @ts-ignore
-    const res = (await highlightQuadrilateral(imageUrl));
-    // console.log(res,'res')
-    // @ts-ignore
-    const highlighted = res.url;
-    // console.log(imageUrl,'lkl')
-    if (imageUrl) {
-      setHighlightedImage(highlighted)
-      // @ts-ignore
-      // const imageUrl = URL.createObjectURL(imageBlob);
-      imageUrl = (await highlightQuadrilateral(imageUrl,false)).url;
-      if (imageUrl) {
-        setPreviewImage(imageUrl); // Set image for preview
-      }
-      // No need to close the modal here, as we're going to show the preview
-    }
-  }, []);
+  const [previewImage, setPreviewImage] = useState<string | null>(null); // For single image preview
+  const [capturedImages, setCapturedImages] = useState<string[]>([]); // For storing URLs in multiple capture mode
+  const [resolveCapture, setResolveCapture] = useState<(files: File[] | null) => void>(() => {});
+  const [multiple, setMultiple] = useState(false); // Flag to indicate if multiple capture mode is enabled
 
-  const openCamera = useCallback(() => {
-    setPreviewImage(null); // Reset preview image
+  const handleCapture = useCallback(async () => {
+    const imageUrl = await cameraRef.current?.takePhoto();
+    if (imageUrl) {
+      // Process for highlighting, if enabled
+      let processedImageUrl = imageUrl;
+      if (enableQuadrilateralHighlighting) {
+        const res = await highlightQuadrilateral(imageUrl);
+        processedImageUrl = res.url;
+      }
+      
+      if (multiple) {
+        // In multiple capture mode, add to the array
+        setCapturedImages(prev => [...prev, processedImageUrl]);
+      } else {
+        // For single capture, just set the preview image
+        setPreviewImage(processedImageUrl);
+      }
+    }
+  }, [enableQuadrilateralHighlighting, multiple]);
+
+  const openCamera = useCallback((isMultiple = true) => {
+    setMultiple(isMultiple);
     setIsModalVisible(true);
-    return new Promise<Blob | null>((resolve) => {
+    setPreviewImage(null); // Ensure preview is reset every time camera is opened
+    setCapturedImages([]); // Always reset for a new session
+    return new Promise<File[] | null>((resolve) => {
       setResolveCapture(() => resolve);
     });
   }, []);
 
-  const handleAccept = useCallback(() => {
-    if (previewImage) {
-      fetch(previewImage)
-        .then((res) => res.blob())
-        .then(blob => compressImage(blobToFile(blob), {
-          maxWidth: 1240,maxHeight: 1754,quality:1
-        }))
-        .then((compressedFile) => {
-          resolveCapture(compressedFile);
-          URL.revokeObjectURL(previewImage); // Clean up
-          setIsModalVisible(false);
-          setPreviewImage(null);
-        });
+  const handleAccept = useCallback(async () => {
+    // Handle accept for single image mode
+    if (previewImage && !multiple) {
+      const response = await fetch(previewImage);
+      const blob = await response.blob();
+      const file = await compressImage(blobToFile(blob, 'captured-image.jpg', 'image/jpeg'), {
+        maxWidth: 1240, maxHeight: 1754, quality: 1
+      });
+      resolveCapture([file]); // Ensure to resolve with an array for consistency
+      setIsModalVisible(false);
+      setPreviewImage(null); // Reset after processing
     }
-  }, [previewImage, resolveCapture]);
+  }, [previewImage, multiple]);
+
+  const handleDone = useCallback(async () => {
+    // Finalize multiple captures
+    if (multiple && capturedImages.length > 0) {
+      const files = await Promise.all(capturedImages.map(async (imageUrl) => {
+        const response = await fetch(imageUrl);
+        const blob = await response.blob();
+        return blobToFile(blob, 'captured-image.jpg', 'image/jpeg');
+      }));
+      resolveCapture(files);
+      setIsModalVisible(false);
+      setCapturedImages([]); // Reset after finalizing
+    }
+  }, [capturedImages, multiple]);
 
   const handleCancel = useCallback(() => {
-    URL.revokeObjectURL(previewImage!); // Clean up
+    // Handle cancel for single image mode
     setPreviewImage(null);
-  }, [previewImage]);
+    setIsModalVisible(false);
+  }, []);
 
   return (
-    // @ts-ignore
     <CameraContext.Provider value={{ openCamera }}>
       {children}
-      <Modal closable={false}
-        visible={isModalVisible}
-        footer={null}
-        // onCancel={() => setIsModalVisible(false)}
-        bodyStyle={{ textAlign: 'center', padding: 0,position:'fixed',top:0,bottom:0,left:0,right:0 }}
-      >
-        <Button danger shape='circle' icon={<CloseOutlined/>} style={{position:'fixed',top:10,right:10,zIndex:1000}} onClick={() => {
-          setIsModalVisible(false)
-        }} />
-        {/* @ts-ignore */}
-        {!previewImage && <Camera facingMode={facingMode} ref={cameraRef} />}
-        {previewImage && (
-          <div style={{position:'relative'}}>
-            <Alert showIcon icon={<WarningOutlined style={{ fontSize: 20 }} />}
-              style={{ position: 'absolute', height: 40, top: 10, right: 10, left: 10, width: '90%' }}
-              type='error'
-              message='The OMR sheet boundary must be highlighted in red' />
- {highlightedImage?<img src={highlightedImage} alt="Captured" style={{ width: '100%', height: 'auto' }} />:null}
-            <Button shape='circle' danger icon={<CloseOutlined/>} style={{position:'absolute',width:40,height:40,bottom:44,left:'30%'}} onClick={handleCancel}></Button>
-            <Button shape='circle' type='primary' icon={<CheckOutlined/>} style={{position:'absolute',width:40,height:40,bottom:44,right:'30%'}} onClick={handleAccept}></Button>
+      <Modal closable={false} visible={isModalVisible} footer={null} onCancel={handleCancel} bodyStyle={{ textAlign: 'center', padding: 0, position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
+        <Button shape='circle' danger icon={<CloseOutlined />} style={{ position: 'fixed', top: 10, right: 10, zIndex: 1000 }} onClick={handleCancel} />
+        {!previewImage && <Camera facingMode="environment" ref={cameraRef} />}
+        {previewImage && !multiple && (
+          <div style={{ position: 'relative' }}>
+            <img src={previewImage} alt="Preview" style={{ width: '100%', height: 'auto' }} />
+            <Button shape='circle' icon={<CloseOutlined />} style={{ position: 'absolute', width: 40, height: 40, bottom: 44, left: '30%' }} onClick={handleCancel}></Button>
+            <Button shape='circle' icon={<CheckOutlined />} style={{ position: 'absolute', width: 40, height: 40, bottom: 44, right: '30%' }} onClick={handleAccept}></Button>
           </div>
         )}
-        {!previewImage && <Button icon={<CameraOutlined/>} style={{
-          position: 'absolute',
-          transform: `translateX(-50%)`,
-          height: 55,
-          width:55,
-        bottom: 20,left:'50%'}} size='large' type="primary" shape='circle' onClick={handleCapture}></Button>}
+        {!previewImage && <Button icon={<CameraOutlined />} style={{ position: 'absolute', left: '50%', bottom: 20, transform: 'translateX(-50%)' }} size="large" type="primary" shape="circle" onClick={handleCapture} />}
+        {multiple && capturedImages.length > 0 && (
+          <>
+            <div style={{ position: 'absolute', left: 10, bottom: 100, maxHeight: '300px', overflowY: 'auto' }}>
+              {capturedImages.map((image, index) => (
+                <img key={index} src={image} alt="Thumbnail" style={{ width: '100px', marginRight: '5px' }} />
+              ))}
+            </div>
+            <Button shape='circle' icon={<CheckOutlined />} style={{ position: 'absolute', right: 10, bottom: 20 }} onClick={handleDone} />
+          </>
+        )}
       </Modal>
     </CameraContext.Provider>
   );
 };
 
-// useCamera hook
 export const useCamera = () => {
   const context = useContext(CameraContext);
   if (!context) {
