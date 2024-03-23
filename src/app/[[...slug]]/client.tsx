c834e0724074dc140a11749959c757d53230d466
Ritesh Ganjewala <ritesh.ganjewala@razorpay.com>
2024-03-23 17:30:52 +0530

    react-query downgrade

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -2,20 +2,9 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 
 const App = dynamic(() => import("../../App"), { ssr: false });
 
 export function ClientOnly() {
-  const queryClient = new QueryClient();
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <App />
-    </HydrationBoundary>
-  );
+  return <App />;
 }
