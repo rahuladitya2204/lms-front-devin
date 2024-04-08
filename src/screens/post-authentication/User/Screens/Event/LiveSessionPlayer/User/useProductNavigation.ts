be083c0f0e931d28de59959010dda6c46749ee07
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-04-08 16:49:16 +0530

    fixed test review player

--- src/screens/post-authentication/User/Screens/Event/LiveSessionPlayer/User/useProductNavigation.ts
+++ src/screens/post-authentication/User/Screens/Event/LiveSessionPlayer/User/useProductNavigation.ts
@@ -4,7 +4,7 @@ import { useNavigate, useParams } from '@Router/index'
 import { Types } from '@adewaskar/lms-common'
 
 function useTestNavigation(test: Types.Test) {
-  const { questionId: itemId } = useParams()
+  const { questionId: itemId,testId } = useParams()
   const navigate = useNavigate()
   const [currentIndexes, setCurrentIndexes] = useState({
     sectionIndex: -1,
@@ -53,7 +53,7 @@ function useTestNavigation(test: Types.Test) {
       if (isIndexValid) {
         const newSectionId = test.sections[sectionIndex]._id
         const newItemId = test.sections[sectionIndex].items[itemIndex]._id
-        navigate(`../${newItemId}`)
+        navigate(`/app/test/${testId}/review/${newItemId}`)
       }
     },
     [currentIndexes, test.sections, navigate, test._id]
