0a9b3414b3851ef5484fb527e2acc9f4ab815631
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-04-11 01:52:58 +0530

    Fixed a few issue in meta

--- src/components/OrgLogo.tsx
+++ src/components/OrgLogo.tsx
@@ -1,47 +1,49 @@
-import { Common, Store, User } from '@adewaskar/lms-common'
+import { Common, Learner, Store, User } from "@adewaskar/lms-common";
 
-import Image from './Image'
-import { Skeleton,Space } from 'antd'
-import { Typography } from '@Components/Typography'
-const { Text } = Typography
+import Image from "./Image";
+import { Skeleton, Space } from "antd";
+import { Typography } from "@Components/Typography";
+const { Text } = Typography;
 interface OrgLogoPropsI {
-  quality?: 'low' | 'high';
+  quality?: "low" | "high";
   noLoadNoShow?: boolean;
   width?: string | number;
   style?: any;
   onClick?: Function;
-  showName?:boolean;
+  showName?: boolean;
 }
 
 function OrgLogo(props: OrgLogoPropsI) {
   // const navigate = useNavigate()
-  const { data: organisation } = Common.Queries.useGetOrgDetails()
-  const logo = organisation?.branding?.logo
+  const { data: organisation } = Learner.Queries.useGetOrgDetails();
+  const logo = organisation?.branding?.logo;
   const SkeletonButton = (
     <Skeleton.Avatar active style={{ width: 45, height: 45 }} />
-  )
+  );
   if (!logo) {
-    return null
+    return null;
   }
   return !organisation._id ? (
     SkeletonButton
   ) : (
     <Space>
-     {/* @ts-ignore */}
-    <Image
-      noLoadNoShow
-      noLoadNoShowPlaceholder={organisation._id ? SkeletonButton : <span />}
-      // onClick={() => navigate('../app/store')}
-      style={{ cursor: 'pointer', margin: 'auto', ...(props.style || {}) }}
-      width={props.width || `45px`}
-      preview={false}
-      // @ts-ignore
-      src={logo[props.quality ? props.quality : 'low'].url}
-      {...props}
-    />
-    {props.showName?<Text style={{fontSize:26}}>{organisation.shortName}</Text>:null}
+      {/* @ts-ignore */}
+      <Image
+        noLoadNoShow
+        noLoadNoShowPlaceholder={organisation._id ? SkeletonButton : <span />}
+        // onClick={() => navigate('../app/store')}
+        style={{ cursor: "pointer", margin: "auto", ...(props.style || {}) }}
+        width={props.width || `45px`}
+        preview={false}
+        // @ts-ignore
+        src={logo[props.quality ? props.quality : "low"].url}
+        {...props}
+      />
+      {props.showName ? (
+        <Text style={{ fontSize: 26 }}>{organisation.name}</Text>
+      ) : null}
     </Space>
-    )
+  );
 }
 
-export default OrgLogo
+export default OrgLogo;
