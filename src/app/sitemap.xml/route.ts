a358ce7d5e7f48102a1597ec951946282013cecf
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-04-11 00:41:49 +0530

    Dynamic sitemap done

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -6,13 +6,13 @@ export async function GET(request:Request) {
     const req = new URL(request.url);
 const { protocol, host }=req;
 const hostParts = host.split('.');
-
+const apiUrl=process.env.API_URL;
 let subdomain = '';
 if (hostParts.length > 2) {
   subdomain = hostParts[0];
 }
   // Fetch the URLs from your API
-  const {data: urls} = await axios.get(`http://localhost:4000/web/sitemap?subdomain=${subdomain}`);
+  const {data: urls} = await axios.get(`${apiUrl}/web/sitemap?subdomain=${subdomain}`);
   const currentDomain = `${protocol}//${host}`;
   console.log(urls,host)
   // Generate the sitemap XML
