c675875f911b1868e79ec391a3b2e9674a89c13a
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-04-10 21:10:59 +0530

    Added overview fallback

---
+++ src/app/package/[id]/page.tsx
@@ -0,0 +1,30 @@
+import Hydrator from "@ServerComponents/Hydrator";
+import { Learner } from "@adewaskar/lms-common";
+import LearnerRootScreen from "@Learner/Screens/LearnerRoot/LearnerRootScreen";
+import ProductCategoryDetailScreen from "@Learner/Screens/StoreScreen/ProductCategoryDetail/ProductCategoryDetail";
+import { getToken } from "@Network/index";
+import ProductCategoryTabs from "@Screens/post-authentication/Learner/Screens/StoreScreen/ProductCategoryDetail/ProductCategoryTabs";
+import PackageDetailsTabs from "@Screens/post-authentication/Learner/Screens/Products/Package/PackageDetailsViewer/PackageDetailTabs";
+
+export default function Page({
+  params,
+}: {
+  params: { type: string; id: string };
+}) {
+  const { getProductCategoryDetails, getPackages } =
+    Learner.Queries.Definitions;
+  return (
+    // @ts-ignore
+    <Hydrator
+      queries={[
+        getProductCategoryDetails(params.id),
+        getPackages(params.id),
+        // getOrgDetails(),
+        // // authenticated routes should only be called if token is present
+        // ...(token ? [getCartDetails(), getLearnerDetails()] : []),
+      ]}
+    >
+      <PackageDetailsTabs isServer type={"overview"} id={params.id} />
+    </Hydrator>
+  );
+}
