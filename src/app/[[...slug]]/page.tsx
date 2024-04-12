84c6cb0ca3edc43f60237dacba00786fb8da2ebb
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-04-12 23:52:11 +0530

    hid footer

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,9 +2,9 @@ import "../../index.css";
 
 import { ClientOnly } from "./client";
 import HomePage from "../home/page";
-export function generateStaticParams() {
-  return [];
-}
+// export function generateStaticParams() {
+//   return [];
+// }
 
 export default function Page({ params }: { params: { slug: string[] } }) {
   if (!params.slug) {
