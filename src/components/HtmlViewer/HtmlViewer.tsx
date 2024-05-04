02f08bb1908f4c27cd85e10da7839dc4e2ae7ed6
Aditya Dewaskar <adityadewaskar@192.168.1.10>
2024-05-04 12:16:11 +0530

    Moved title to slug

--- src/components/HtmlViewer/HtmlViewer.tsx
+++ src/components/HtmlViewer/HtmlViewer.tsx
@@ -23,43 +23,4 @@ function HtmlViewer(props: { content: string; noPreviewImage?: boolean }) {
   );
 }
 
-const cleanContent = (
-  htmlContent: string,
-  tags = ["div", "span", "p", "svg", "path", "li"]
-): string => {
-  const getDOMParser = () => {
-    if (typeof window === "undefined") {
-      // Server-side (Node.js)
-      const { JSDOM } = require("jsdom");
-      return new JSDOM().window.DOMParser;
-    } else {
-      // Client-side (browser)
-      return window.DOMParser;
-    }
-  };
-
-  const DOMParser = getDOMParser();
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlContent, "text/html");
-
-  const isEmptyNode = (node: Node): boolean => {
-    return (
-      node.childNodes.length === 0 && (node as Element).attributes.length === 0
-    );
-  };
-
-  const cleanNode = (node: Node): void => {
-    Array.from(node.childNodes).forEach((child) => {
-      cleanNode(child);
-      if (tags.includes(child.nodeName.toLowerCase()) && isEmptyNode(child)) {
-        node.removeChild(child);
-      }
-    });
-  };
-
-  cleanNode(doc.body);
-
-  return doc.body.innerHTML;
-};
-
 export default HtmlViewer;
