88c5d1afabc6682ff188daa97d790f331a761372
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-05-09 01:05:02 +0530

    exam upsc test series

--- src/app/test-series/[slug]/page.tsx
+++ src/app/test-series/[slug]/page.tsx
@@ -7,10 +7,120 @@ import { getToken } from "@Network/index";
 import PackagesList from "@Screens/post-authentication/Learner/Screens/Products/Package/PackagesList/PackagesListScreen";
 import axios from "axios";
 import { getCookie } from "@ServerUtils/index";
-import LearnerRootScreen from "@Screens/post-authentication/Learner/Screens/LearnerRoot/LearnerRootScreen";
-const apiUrl = process.env.API_URL;
+import { Metadata } from "next";
 
-export const generateMetadata = GenerateMetadata;
+export async function generateMetadata(req: {
+  params: any;
+  searchParams: any;
+  headers: Headers;
+}): Promise<Metadata> {
+  console.log(req, "reqqq");
+  const alias = getCookie("orgAlias")?.split("-")[0];
+  const userType = getCookie("userType");
+  const slug = req.params.slug;
+  const type = req.params.type || "overview";
+  // console.log(alias, userType, id, "kututurur");
+  if (alias && userType && slug) {
+    const apiUrl = process.env.API_URL;
+    const { data: category }: { data: Types.ProductCategory } = await axios(
+      `${apiUrl}/learner/product-category/slug/${slug}`,
+      {
+        headers: {
+          "x-org-alias": alias,
+        },
+      }
+    );
+    const url = `https://${alias}.testmint.ai/test-series/${slug}`;
+    return {
+      title: category.testSeries?.seo?.meta?.title,
+      description: category.testSeries?.seo?.meta?.description,
+      // icons: {
+      //   icon: bundle.thumbnailImage,
+      //   apple: bundle.thumbnailImage,
+      // },
+      viewport: "width=device-width, initial-scale=1",
+      themeColor: "#ffffff",
+      manifest: "/manifest.json",
+      openGraph: {
+        title: category.testSeries?.seo?.meta?.title,
+        description: category.testSeries?.seo?.meta?.description,
+        type: "website",
+        locale: "en_IN",
+        url: url,
+        images: [
+          {
+            url: category.thumbnailImage,
+            width: 800,
+            height: 600,
+            alt: category.title,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: category.title,
+        description: category.title,
+        images: [category.thumbnailImage],
+      },
+      alternates: {
+        canonical: url,
+      },
+      other: {
+        // "application/ld+json": JSON.stringify({
+        //   "@context": "https://schema.org",
+        //   "@type": "WebPage",
+        //   name: bundle.title,
+        //   description: bundle.title,
+        //   url: url,
+        // }),
+      },
+    };
+  }
+
+  return {
+    title: "Testmint",
+    description: "Testmint",
+    icons: {
+      icon: "/favicon.ico",
+      apple: "/apple-touch-icon.png",
+    },
+    viewport: "width=device-width, initial-scale=1",
+    themeColor: "#ffffff",
+    manifest: "/manifest.json",
+    openGraph: {
+      title: "Testmint",
+      description: "Testmint",
+      type: "website",
+      url: "https://testmint.ai",
+      images: [
+        {
+          url: "/og-image.png",
+          width: 800,
+          height: 600,
+          alt: "Testmint",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Testmint",
+      description: "Testmint",
+      images: ["/twitter-image.png"],
+    },
+    alternates: {
+      canonical: "https://testmint.ai",
+    },
+    other: {
+      "application/ld+json": JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        name: "Testmint",
+        description: "Testmint",
+        url: "https://testmint.ai",
+      }),
+    },
+  };
+}
 
 export default function Page({ params }: { params: { slug: string } }) {
   const {
