375efa6e7fccfa98241592653e1184db8be98665
Aditya Dewaskar <adityadewaskar@Adityas-MacBook-Air.local>
2024-05-16 00:56:07 +0530

    Added parsehtml

--- src/components/HtmlViewer/HtmlViewer.tsx
+++ src/components/HtmlViewer/HtmlViewer.tsx
@@ -1,11 +1,9 @@
 import "./suneditor.css";
+import { List, Typography } from "antd";
+import parse, { domToReact } from "html-react-parser";
+import { Element } from "domhandler";
 
-import AppImage from "@Components/Image";
-import AudioPlayer from "@Components/AudioPlayer";
-import { Image } from "antd";
-import MediaPlayer from "@Components/MediaPlayer/MediaPlayer";
-import parse from "html-react-parser";
-import dynamic from "next/dynamic";
+const { Title, Paragraph } = Typography;
 
 function HtmlViewer(props: { content: string; noPreviewImage?: boolean }) {
   // @ts-ignore
@@ -13,12 +11,55 @@ function HtmlViewer(props: { content: string; noPreviewImage?: boolean }) {
   if (!children) {
     return null;
   }
+
+  const convertNodeToElement = (node: Element | Text, index: number) => {
+    if (node.type === "tag") {
+      switch (node.name) {
+        case "h1":
+        case "h2":
+        case "h3":
+        case "h4":
+        case "h5":
+        case "h6":
+          const level = parseInt(node.name.slice(1));
+          return (
+            <Title key={index} level={level}>
+              {domToReact(node.children)}
+            </Title>
+          );
+        case "p":
+          return <Paragraph key={index}>{domToReact(node.children)}</Paragraph>;
+        case "ul":
+        case "ol":
+          return (
+            <List key={index}>
+              {node.children.map((child, childIndex) => {
+                if (child.type === "tag" && child.name === "li") {
+                  return (
+                    <List.Item key={childIndex}>
+                      {domToReact(child.children)}
+                    </List.Item>
+                  );
+                }
+                return null;
+              })}
+            </List>
+          );
+        // Add more cases for other HTML tags you want to convert
+        default:
+          return <div key={index}>{domToReact(node.children)}</div>;
+      }
+    } else if (node.type === "text") {
+      return <Paragraph key={index}>{node.data}</Paragraph>;
+    }
+    return null;
+  };
+
   return (
     <div className="ck ck-content ck-editor__editable ck-rounded-corners ck-editor__editable_inline ck-blurred">
-      <div
-        className="html-viewer"
-        dangerouslySetInnerHTML={{ __html: children }}
-      ></div>
+      <div className="html-viewer">
+        {parse(children, { replace: convertNodeToElement })}
+      </div>
     </div>
   );
 }
