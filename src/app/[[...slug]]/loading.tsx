f1388b5d6d133d15d82bbff719059165eebed301
Aditya Dewaskar <adityadewaskar@192.168.1.6>
2024-05-26 11:55:27 +0530

    Added loading screen

---
+++ src/app/[[...slug]]/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import LearnerRootScreenSkeleton from "@Screens/post-authentication/Learner/Screens/LearnerRoot/LearnerRootScreenSkeleton";
+import HomeScreenSkeleton from "@Screens/post-authentication/Learner/Screens/StoreScreen/HomeScreen/HomeScreenSkeleton";
+
+export default function Loading() {
+  return (
+    <LearnerRootScreenSkeleton>
+      <HomeScreenSkeleton />
+    </LearnerRootScreenSkeleton>
+  );
+}
