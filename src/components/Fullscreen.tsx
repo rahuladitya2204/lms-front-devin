551c963cec336624f0ca91db6af15cf1c01cfad2
Aditya Dewaskar <adityadewaskar@192.168.1.6>
2024-05-26 21:26:05 +0530

    Working well camera

--- src/components/Fullscreen.tsx
+++ src/components/Fullscreen.tsx
@@ -44,11 +44,9 @@ const Fullscreen = ({ children }) => {
   }
 
   return (
-    <Layout.Content>
-      <div ref={fullscreenRef} style={{ width: "100%", height: "100%" }}>
-        {children}
-      </div>
-    </Layout.Content>
+    <div ref={fullscreenRef} style={{ width: "100%", height: "100%" }}>
+      {children}
+    </div>
   );
 };
 
