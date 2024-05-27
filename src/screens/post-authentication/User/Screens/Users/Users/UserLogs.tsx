b6e6b247544ef3a832dadc986dfa92b84d8f9409
Aditya Dewaskar <adityadewaskar@192.168.1.6>
2024-05-27 20:46:43 +0530

    Fixed alot in monitoring

--- src/screens/post-authentication/User/Screens/Users/Users/UserLogs.tsx
+++ src/screens/post-authentication/User/Screens/Users/Users/UserLogs.tsx
@@ -2,51 +2,59 @@ import AppImage from "@Components/Image";
 import Table, { TableColumn } from "@Components/Table/TableComponent";
 import { Title } from "@Components/Typography/Typography";
 import { Types, User } from "@adewaskar/lms-common";
-import { Col, Divider, Image, Row, Space } from "antd";
+import { Col, Divider, Image, Row, Space, Spin } from "antd";
 import dayjs from "dayjs";
 
 export default function UserLogs({ id }: { id: string }) {
-  const { data: userLog } = User.Queries.useGetUserLog(id);
-  console.log(userLog, "userLog");
+  const { data: userLog, isLoading } = User.Queries.useGetUserLog(id);
+  // console.log(userLog, "userLog");
   return (
-    <Row>
-      <Col span={24}>
-        <Title level={4}>
-          Started At: {dayjs(userLog.startedAt).format("LLL")}
-        </Title>
-        <Title level={4}>
-          Last Updated At: {dayjs(userLog.lastUpdate).format("LLL")}
-        </Title>
-        <Title level={4}>
-          Total Time Logged:{" "}
-          {formatDuration(userLog.startedAt, userLog.lastUpdate)}
-        </Title>
-      </Col>
-      <Col span={24}>
-        <Table dataSource={userLog.screenshots}>
-          <TableColumn
-            render={(
-              _: any,
-              record: { date: string; message: string; url: string }
-            ) => <Space size="middle">{dayjs(record.date).format("LL")}</Space>}
-          />
+    <Spin spinning={isLoading}>
+      <Row>
+        {userLog._id ? (
+          <Col span={24}>
+            <Title level={4}>
+              Started At: {dayjs(userLog.startedAt).format("LLL")}
+            </Title>
+            <Title level={4}>
+              Last Updated At: {dayjs(userLog.lastUpdate).format("LLL")}
+            </Title>
+            <Title level={4}>
+              Total Time Logged:{" "}
+              {formatDuration(userLog.startedAt, userLog.lastUpdate)}
+            </Title>
+          </Col>
+        ) : null}
+        <Col span={24}>
+          <Image.PreviewGroup>
+            <Table dataSource={userLog.screenshots}>
+              <TableColumn
+                render={(
+                  _: any,
+                  record: { date: string; message: string; url: string }
+                ) => (
+                  <Space size="middle">{dayjs(record.date).format("LL")}</Space>
+                )}
+              />
 
-          <TableColumn
-            render={(
-              _: any,
-              record: { date: string; message: string; url: string }
-            ) => record.message}
-          />
+              <TableColumn
+                render={(
+                  _: any,
+                  record: { date: string; message: string; url: string }
+                ) => record.message}
+              />
 
-          <TableColumn
-            render={(
-              _: any,
-              record: { date: string; message: string; url: string }
-            ) => <Image preview width={70} height={70} src={record.url} />}
-          />
-        </Table>
-      </Col>
-    </Row>
+              <TableColumn
+                render={(
+                  _: any,
+                  record: { date: string; message: string; url: string }
+                ) => <Image preview width={50} height={50} src={record.url} />}
+              />
+            </Table>
+          </Image.PreviewGroup>
+        </Col>
+      </Row>
+    </Spin>
   );
 }
 
