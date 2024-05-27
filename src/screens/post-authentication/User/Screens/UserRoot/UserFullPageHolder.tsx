4607d31af1e42a2f9c0af8753e3a9edfb38f7136
Aditya Dewaskar <adityadewaskar@192.168.1.6>
2024-05-27 16:28:16 +0530

    Screenshot effect added

--- src/screens/post-authentication/User/Screens/UserRoot/UserFullPageHolder.tsx
+++ src/screens/post-authentication/User/Screens/UserRoot/UserFullPageHolder.tsx
@@ -1,27 +1,30 @@
-import Layout from '@Components/Layout'
-import { ModalProvider } from '@Components/ActionModal/ModalContext'
-import { Outlet } from 'react-router'
-import ProtectedContent from '@Components/ProtectedComponent'
-import { Store } from '@adewaskar/lms-common'
-import ThemeProvider from 'screens/ThemeProvider'
-import { UserLogin } from '../Login'
+import Layout from "@Components/Layout";
+import { ModalProvider } from "@Components/ActionModal/ModalContext";
+import { Outlet } from "react-router";
+import ProtectedContent from "@Components/ProtectedComponent";
+import { Store } from "@adewaskar/lms-common";
+import ThemeProvider from "screens/ThemeProvider";
+import { UserLogin } from "../Login";
+import MonitoringComponent from "@Components/monitoring/MonitoringComponent";
 
-export default function UserFullPageHolder () {
-  const { isSignedIn } = Store.useAuthentication(s => s)
+export default function UserFullPageHolder() {
+  const { isSignedIn } = Store.useAuthentication((s) => s);
   return (
     <ThemeProvider>
       <ModalProvider>
-        <Layout style={{ minHeight: '100vh' }}>
-          <ProtectedContent
-            width={300}
-            title="Login"
-            cta={<UserLogin />}
-            isVerified={isSignedIn}
-          >
-            <Outlet />
-          </ProtectedContent>
-        </Layout>
+        <MonitoringComponent>
+          <Layout style={{ minHeight: "100vh" }}>
+            <ProtectedContent
+              width={300}
+              title="Login"
+              cta={<UserLogin />}
+              isVerified={isSignedIn}
+            >
+              <Outlet />
+            </ProtectedContent>
+          </Layout>
+        </MonitoringComponent>
       </ModalProvider>
     </ThemeProvider>
-  )
+  );
 }
