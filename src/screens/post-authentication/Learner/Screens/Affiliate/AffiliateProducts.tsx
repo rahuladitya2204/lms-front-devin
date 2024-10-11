f2d805372eeb92ab4c14a1ae17fa8d59d181889a
Aditya Dewaskar <adityadewaskar@192.168.1.9>
2024-10-11 11:22:17 +0530

    Fied copy link

--- src/screens/post-authentication/Learner/Screens/Affiliate/AffiliateProducts.tsx
+++ src/screens/post-authentication/Learner/Screens/Affiliate/AffiliateProducts.tsx
@@ -1,35 +1,35 @@
-import { Button, Card, Col, Empty, Row, Spin } from 'antd'
-import { Enum, Learner, Types } from '@adewaskar/lms-common'
+import { Button, Card, Col, Empty, Row, Spin } from "antd";
+import { Enum, Learner, Types } from "@adewaskar/lms-common";
 
-import AppImage from '@Components/Image'
-import { CheckOutlined } from '@ant-design/icons'
-import Tabs from '@Components/Tabs'
-import { Typography } from '@Components/Typography'
-import { copyToClipboard } from '@Utils/index'
-import { useState } from 'react'
+import AppImage from "@Components/Image";
+import { CheckOutlined, CopyOutlined } from "@ant-design/icons";
+import Tabs from "@Components/Tabs";
+import { Typography } from "@Components/Typography";
+import { copyToClipboard } from "@Utils/index";
+import { useMemo, useState } from "react";
 
-const { Text } = Typography
+const { Text } = Typography;
 export default function AffiliateProducts() {
-  const tabs = []
+  const tabs = [];
   tabs.push(
     {
-      label: 'Tests',
-      key: 'tests',
-      children: <ProductList type="test" />
-    },
-    {
-      label: 'Events',
-      key: 'events',
-      children: <ProductList type="event" />
-    },
-    {
-      label: 'Courses',
-      key: 'courses',
-      children: <ProductList type="course" />
+      label: "Test Series",
+      key: "test-series",
+      children: <ProductList type="package" />,
     }
-  )
+    // {
+    //   label: 'Events',
+    //   key: 'events',
+    //   children: <ProductList type="event" />
+    // },
+    // {
+    //   label: 'Courses',
+    //   key: 'courses',
+    //   children: <ProductList type="course" />
+    // }
+  );
 
-  return <Tabs type="card" items={tabs} />
+  return <Tabs tabKey="products" type="card" items={tabs} />;
 }
 
 interface ProductCardPropsI {
@@ -38,24 +38,37 @@ interface ProductCardPropsI {
 }
 
 export const ProductCard = (props: ProductCardPropsI) => {
-  const [copy, setCopy] = useState(false)
-  const { product } = props
-  const {
-    data: affiliateDetails
-  } = Learner.Queries.useGetAffiliateAccountDetails()
+  const [copy, setCopy] = useState(false);
+  const { product } = props;
+  const { data: affiliateDetails } =
+    Learner.Queries.useGetAffiliateAccountDetails();
+  const type = useMemo(() => {
+    switch (props.type) {
+      case "package":
+        return "test-series";
+      case "course":
+        return "course";
+      case "event":
+        return "event";
+      default:
+        return "test";
+    }
+  }, [props.type]);
   //   @ts-ignore
-  const url = `${window.location.host}/app/${props.type}/${product._id}?ref=${
+  const url = `${window.location.host}/${type}/${
+    product.slug || product._id
+  }?ref=${
     // @ts-ignore
     affiliateDetails._id
-  }`
+  }`;
   const generateLink = () => {
-    setCopy(true)
-    setTimeout(() => setCopy(false), 5000)
-    copyToClipboard(url)
-  }
+    setCopy(true);
+    setTimeout(() => setCopy(false), 5000);
+    copyToClipboard(url);
+  };
   return (
     <Card
-      bodyStyle={{ padding: '20px 10px' }}
+      bodyStyle={{ padding: "20px 10px" }}
       cover={
         <AppImage height={80} alt="example" src={product.thumbnailImage} />
       }
@@ -63,29 +76,27 @@ export const ProductCard = (props: ProductCardPropsI) => {
       <Card.Meta
         description={
           <Button
-            type={copy ? 'primary' : 'default'}
+            type={copy ? "primary" : "default"}
             block
-            icon={copy ? <CheckOutlined /> : null}
+            icon={copy ? <CheckOutlined /> : <CopyOutlined />}
             onClick={generateLink}
-            size="small"
+            // size="small"
           >
-            {copy ? 'Link Copied' : 'Copy Link'}
+            {copy ? "Link Copied" : "Copy Link"}
           </Button>
         }
         title={<Text>{product.title}</Text>}
       />
     </Card>
-  )
-}
+  );
+};
 
 export const ProductList = (props: {
   //   products: Types.ProductItem[],
-  type: string
+  type: string;
 }) => {
-  const {
-    data: products,
-    isLoading: loadingProducts
-  } = Learner.Queries.useGetProducts(props.type)
+  const { data: products, isLoading: loadingProducts } =
+    Learner.Queries.useGetProducts(props.type);
 
   return (
     <Spin spinning={loadingProducts}>
@@ -93,13 +104,13 @@ export const ProductList = (props: {
         <Empty />
       ) : (
         <Row gutter={[20, 30]}>
-          {products.map(product => (
-            <Col xs={24} sm={12} md={6} lg={4}>
+          {products.map((product) => (
+            <Col xs={24} sm={12} md={12} lg={6}>
               <ProductCard type={props.type} product={product} />
             </Col>
           ))}
         </Row>
       )}
     </Spin>
-  )
-}
+  );
+};
